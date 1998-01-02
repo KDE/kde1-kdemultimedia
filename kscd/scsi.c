21e3c92d84b72ea0b0b8be735965565b2875e65c
KDE Dinosaur wuebben <cvsuser@kde.org>
1998-01-02 07:39:05 +0000

    1

--- kscd/scsi.c
+++ kscd/scsi.c
@@ -1,3 +1,27 @@
+/*   
+   Kscd - A simple cd player for the KDE Project
+
+   $Id$
+ 
+   Copyright (c) 1997 Bernd Johannes Wuebben math.cornell.edu
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+ */
+
 /*
  * @(#)scsi.c	1.8	3/3/94
  *
@@ -236,7 +260,7 @@ wm_scsi2_play(d, sframe, eframe)
 
 /* pretty much the same as wm_scsi2_get_trackinf below
    it fetches the min, sec, and frm adderss of the beginning of 
-  the track d
+   the track d. (BERND)
   */
 
 int
