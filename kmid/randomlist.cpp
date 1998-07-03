47c081689d76a5dd3ba554b437ce1d0f733dd319
KDE Dinosaur garbanzo <cvsuser@kde.org>
1998-07-03 21:16:20 +0000

    Impart some formatting sanity upon this file, and fix some stuff as reported by -Werror.  However, there's still some questionable stuff.  I hope I didn't step on anyone's toes.

--- kmid/randomlist.cpp
+++ kmid/randomlist.cpp
@@ -21,82 +21,91 @@
     or to Antonio Larrosa, Rio Arnoya, 10 5B, 29006 Malaga, Spain
 
 ***************************************************************************/
+
+/*
+ * $Id$
+ */
+
 #include "randomlist.h"
+
 #include <stdlib.h>
+#include <stdio.h>
+
 #define RAND_UNIFORM (double)rand()/(double)RAND_MAX
 
-int random_discrete(double *distrib,int n)
+int random_discrete(double *distrib, int n)
 {
-int i=0;
-double g=0.0;
-double z=0.0;
-while ((z==0.0)||(z==1.0)) z=RAND_UNIFORM;
-while ((g<z)&&(i<n))
-   {
-   g+=distrib[i];
-   i++;
-   };
-return i-1;
+    int i=0;
+    double g=0.0, z=0.0;
+    while ((z==0.0)||(z==1.0))
+      z=RAND_UNIFORM;
+    while ((g<z)&&(i<n)) {
+     g+=distrib[i];
+     i++;
+    };
+    return i-1;
 };
 
 double *generate_discrete_uniform_distrib(int n)
 {
-double *distrib=new double[n];
-for (int i=0;i<n;i++)
-   {
-   distrib[i]=1.0/(double)n;
-   };
-return distrib;
-};
-
-void remove_lmn_from_discrete_distrib(int k,double *distrib,int n,int used=0)
-{
-int i;
-if (used==0)  //guess how many values of the v.a. are strictly positive
-    {
-    for (i=0;i<n;i++) if (distrib[i]>0) used++;
+    double *distrib=new double[n];
+    for (int i=0;i<n;i++) {
+      distrib[i]=1.0/(double)n;
     };
-used--; // k will no longer be used :-)
-if (used==0) return;
-double piece=distrib[k]/(double)used;
-distrib[k]=0.0;
-for (i=0;i<n;i++) if (distrib[i]>0) distrib[i]+=piece;
+    return distrib;
 };
 
-void show_distrib(double *distrib,int n)
+void remove_lmn_from_discrete_distrib(int k, double *distrib, int n, int used=0)
 {
-printf("(");
-for (int j=0;j<n;j++) printf("%g,",distrib[j]);
-printf(")\n");
+    int i;
+    double piece;
+
+    if (used==0) { //guess how many values of the v.a. are strictly positive
+      for (i=0;i<n;i++)
+        if (distrib[i]>0)
+          used++;
+    };
+    used--; // k will no longer be used :-)
+    if (used==0)
+      return;
+    piece=distrib[k]/(double)used;
+    distrib[k]=0.0;
+    for (i=0;i<n;i++)
+      if (distrib[i]>0)
+        distrib[i]+=piece;
+};
+
+void show_distrib(double *distrib, int n)
+{
+    printf("(");
+    for (int j=0;j<n;j++)
+      printf("%g,",distrib[j]);
+    printf(")\n");
 };
 
 int *generate_random_list(int n)
 {
-if (n==0) return NULL;
-int *list=new int[n];
-double *distrib=generate_discrete_uniform_distrib(n);
-int used=n;
-int x;
-int i=1;
-while (used>0)
-   {
-   x=random_discrete(distrib,n);
-   list[x]=i;
-   i++;
-   remove_lmn_from_discrete_distrib(x,distrib,n,used);
-   used--;
-   };
-delete distrib;
-
-return list;
+    if (n==0) return NULL;
+    int *list=new int[n];
+    double *distrib=generate_discrete_uniform_distrib(n);
+    int x, i=1, used=n;
+    while (used>0) {
+      x=random_discrete(distrib,n);
+      list[x]=i;
+      i++;
+      remove_lmn_from_discrete_distrib(x,distrib,n,used);
+      used--;
+    };
+    delete distrib;
+    return list;
 };
 
 int *generate_list(int n)
 {
-int *list=new int[n];
-for (int i=0;i<n;i++)
-   {
-   list[i]=i+1;
-   };
-return list;
+    int *list=new int[n];
+
+    for (int i=0;i<n;i++)
+      list[i]=i+1;
+
+    return list;
 };
