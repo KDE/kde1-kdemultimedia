ccd5f3b739f63df851c601254ec30865380e9594
KDE Dinosaur pbrown <cvsuser@kde.org>
1998-12-18 16:22:19 +0000

    can't initialize statics in global scope. fixed.

--- kmidi/dumb_c.c
+++ kmidi/dumb_c.c
@@ -66,10 +66,12 @@ ControlMode ctl=
   ctl_expression, ctl_panning, ctl_sustain, ctl_pitch_bend
 };
 
-static FILE *infp=stdin, *outfp=stdout; /* infp isn't actually used yet */
+static FILE *infp;  /* infp isn't actually used yet */
+static FILE *outfp;
 
 static int ctl_open(int using_stdin, int using_stdout)
 {
+  infp = stdin; outfp = stdout;
   if (using_stdin && using_stdout)
     infp=outfp=stderr;
   else if (using_stdout)
